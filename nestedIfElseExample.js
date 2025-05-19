2411fa6b9db8594e815f50fddae2bcf82a051e26
unknown <misskhat.hossen@gmail.com>
2025-05-19 17:10:03 +0600

    nested if example update

---
+++ nestedIfElseExample.js
@@ -0,0 +1,14 @@
+const money = 300;
+const propcornPrice = 50;
+
+if (money >= 300) {
+    console.log("Going to watch movie");
+    if (propcornPrice <= 50) {
+        console.log("Buying propcorn");
+    } else {
+        console.log("Propcorn is expansive");
+    }
+} else {
+    console.log("Home Alone");
+}
+
