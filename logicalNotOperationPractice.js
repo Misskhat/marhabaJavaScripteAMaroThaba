97c08dcd3ab8f82e4ccc07ca50abd1c5c1d8d890
unknown <misskhat.hossen@gmail.com>
2025-05-24 19:16:21 +0600

    logical not operation practice added

---
+++ logicalNotOperationPractice.js
@@ -0,0 +1,30 @@
+const buyingAmount = 3001;
+let cashBack;
+cashBack = buyingAmount > 3000 ? 500 : 0;
+console.log("Cash Back Amount", cashBack);
+
+let age = 20;
+age > 15 ? console.log('Teenager') : console.log('child');
+
+
+const isLoging = true;
+let msg;
+msg = isLoging ? "Welcome Back" : "Please login";
+console.log(msg);
+
+
+const carTanki = true;
+carTanki ? console.log("Ready for long drive") : console.log("Fill the tank");;
+
+
+const isPassed = true;
+let currentSituation;
+currentSituation = isPassed ? "Party Time" : "Next semester serious";
+console.log(currentSituation)
+
+
+let sunny = "";
+sunny ? console.log("Go Out") : console.log("Stay Home");
+
+const expensive = false;
+expensive ? console.log('Not buy this item') : console.log('I will buy this item');
