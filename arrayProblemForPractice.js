e98fe849b49e346897aac9952e8b21f23aea0317
unknown <misskhat.hossen@gmail.com>
2025-05-24 19:34:56 +0600

    array problem practice added

---
+++ arrayProblemForPractice.js
@@ -0,0 +1,17 @@
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+console.log(numbers);
+
+const freezFruits = ['apple', 'orange', 'banana', 'mango', 'guava'];
+console.log(freezFruits);
+
+const vegitabls = ['alu', 'payag', 'bagun', 'mula', 'garo', 'tomato', 'daros', 'lau', 'sosa', 'potol'];
+console.log(vegitabls.length);
+
+const oddNumber = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+console.log(oddNumber);
+
+const colors = ['blue', 'brown', 'yellow', 'red', 'black', 'marun', 'white', 'silver', 'orange', 'purpale', 'ass', 'skyblue', 'darkblue', 'green'];
+console.log(colors.length);
+
+const mainCity = ['dhaka', 'ho noi', 'shanghai', 'seoul', 'dilli', 'karachi', 'london', 'newyork']
+console.log(mainCity.length)
