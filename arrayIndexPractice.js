97a437e951d91bc6164cfb3b7c44ad9344c597e1
unknown <misskhat.hossen@gmail.com>
2025-05-25 13:45:45 +0600

    array index practice upated

---
+++ arrayIndexPractice.js
@@ -0,0 +1,22 @@
+const friends = ['rakib', 'shipon', 'nayem', 'humayon', 'bappy', 'rabbi'];
+console.log(friends[3]);
+
+const books = ['fera', 'misirali', 'himu', 'opeakha', 'monersokti', 'marhaba maro javascript a maro thaba', 'javascript by zunayed ahmed'];
+console.log(books[5]);
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[7] = 30;
+console.log(numbers);
+
+const games = ['free fire', 'pabg', 'candy crash', 'tample run'];
+games[2] = 'subway surfer';
+console.log(games);
+
+const familyMember = ['abbu', 'ammu', 'ami', 'amar wife', 'soto bhai', 'sobar soto bon'];
+console.log(familyMember[2]);
+
+const tourCountryName = ['nepal', 'vutan', 'thailand', 'singapur', 'maldip', 'mali', 'ajerbijan'];
+console.log(tourCountryName[3]);
+
+const tableItem = ['mobile', 'monitor', 'watch', 'dairy'];
+console.log(tableItem[6]);
