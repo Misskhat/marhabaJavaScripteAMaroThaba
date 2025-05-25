60d59915ec7f474ee91d13d6715ee3a6e6f2bd51
unknown <misskhat.hossen@gmail.com>
2025-05-25 18:34:59 +0600

    push pop method practice update

---
+++ javaScriptPushPopMethodPractice.js
@@ -0,0 +1,21 @@
+const numbers = [10, 20, 30, 40, 50];
+numbers.push(60);
+console.log(numbers);
+
+
+const someFriends = ['sojib', 'sagor', 'sakib', 'sohel'];
+someFriends.push('sumon');
+console.log(someFriends);
+
+
+const mobileGame = ['free fire', 'pabg', 'candy crash', 'tample run'];
+mobileGame.pop();
+console.log(mobileGame);
+
+const newNumbers = [24, 36, 48, 60];
+newNumbers.unshift(12);
+console.log(newNumbers);
+
+const books = ['fera', 'misirali', 'himu', 'opeakha', 'monersokti', 'marhaba maro javascript a maro thaba', 'javascript by zunayed ahmed'];
+books.shift();
+console.log(books);
