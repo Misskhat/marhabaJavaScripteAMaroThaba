499c0e718fec1e7f0d631ad4ccb9b03e89e6830d
unknown <misskhat.hossen@gmail.com>
2025-05-27 18:28:03 +0600

    java script while loop 2nd practice added

---
+++ javaScriptWhileLoop2Practice.js
@@ -0,0 +1,41 @@
+let num50 = 50;
+while (num50 <= 100) {
+    console.log(num50);
+    num50++
+}
+
+let num5 = 5;
+let sum = 0;
+while (num5 <= 15) {
+    sum = sum + num5;
+    num5++;
+}
+
+console.log(sum);
+
+let studentRoll = 1;
+let studentSum = 0;
+while (studentRoll <= 50) {
+    console.log(studentRoll);
+    studentSum += studentRoll;
+    studentRoll++
+}
+
+console.log(studentSum);
+
+
+let num21 = 21;
+let sum21to50 = 0;
+while (num21 <= 50) {
+    console.log(sum21to50 += num21);
+    num21++
+}
+
+let num20 = 20;
+let sum20to40 = 0;
+while (num20 <= 40) {
+    sum20to40 = sum20to40 + num20;
+    num20++
+}
+
+console.log(sum20to40);
