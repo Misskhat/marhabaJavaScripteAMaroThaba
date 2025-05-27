8510dd86dd934d51630aba5e294b56bf0d29ed9d
unknown <misskhat.hossen@gmail.com>
2025-05-27 18:02:24 +0600

    javaScript while practice added

---
+++ javaScriptWhilePractice.js
@@ -0,0 +1,33 @@
+let num = 0;
+while (num < 10) {
+    console.log('ami protidin 3 ghonta programming practice korbo');
+    num++;
+}
+
+
+let counterNumer = 1;
+while (counterNumer <= 20) {
+    console.log(counterNumer);
+    counterNumer++;
+}
+
+
+let number50To100 = 50;
+while (number50To100 <= 100) {
+    console.log(number50To100);
+    number50To100 = number50To100 + 1;
+}
+
+let namta = 1;
+while (namta <= 10) {
+    console.log(namta * 3);
+    namta++
+}
+
+
+// this is wrong problem
+let number111 = 111;
+while (number111 <= 210) {
+    console.log(number111 / 2);
+    number111++
+}
