5b0a9b017d167521b8dc602be2b219c9acbe8824
unknown <misskhat.hossen@gmail.com>
2025-05-27 19:01:48 +0600

    java script for loop practice

---
+++ javaScriptForLoopPractice.js
@@ -0,0 +1,19 @@
+for (let i = 150; i <= 170; i++) {
+    console.log(i);
+}
+
+let studentSum = 0;
+for (let index = 31; index <= 58; index++) {
+    console.log(index);
+    studentSum += index;
+}
+console.log(studentSum);
+
+let sum = 0;
+for (let i = 25; i <= 75; i++) {
+    sum += i;
+    console.log(sum);
+}
+console.log(sum);
+
+
