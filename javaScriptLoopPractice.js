3e50f87fd40b1e0803a53f8a41b3dcb81fab814a
unknown <misskhat.hossen@gmail.com>
2025-05-27 16:02:48 +0600

    javaScript loop practice added

---
+++ javaScriptLoopPractice.js
@@ -0,0 +1,24 @@
+const subject = ['bangla', 'english', 'math', 'accounting', 'finance'];
+for (const sub of subject) {
+    console.log(sub);
+};
+
+const favoritFood = ['kacchi', 'rost', 'beriany', 'khichuri', 'mansho'];
+for (const fav of favoritFood) {
+    console.log(fav);
+}
+
+const familyBairthYear = [1996, 1997, 2000, 2011];
+for (const element of familyBairthYear) {
+    console.log(element);
+}
+
+const favoritPlayer = ['sakib', 'tamim', 'musfique', 'marshafi', 'riyad', 'mahamudulla', 'nasir', 'mustafiz', 'bijoy', 'mominul', 'liton'];
+for (const fav of favoritPlayer) {
+    console.log(fav);
+}
+
+const examDate = [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30];
+for (const ed of examDate) {
+    console.log(ed);
+}
