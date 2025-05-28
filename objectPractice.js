d00ad82c1a96868b4b1e739b18af50e99e7d8455
unknown <misskhat.hossen@gmail.com>
2025-05-28 20:08:03 +0600

    object practice added

---
+++ objectPractice.js
@@ -0,0 +1,55 @@
+const teacher = {
+    name: 'samonl',
+    subject: 'english',
+    age: '50',
+    house: 'dhaka'
+}
+console.log(teacher);
+
+const tree = {
+    treeName: 'mangoTree',
+    hight: '10ft',
+    treeAge: 5,
+    fruitName: 'mango'
+}
+console.log(tree);
+
+const person = {
+    name: 'omar',
+    age: 30,
+    passion: 'youtube',
+    profession: 'job'
+}
+console.log(person);
+
+const father = {
+    name: 'hasmot',
+    age: 60,
+    profession: 'self employed',
+    Children: 5,
+}
+console.log(father);
+
+const motorbike = {
+    name: 'suzuki',
+    tire: 2,
+    color: 'black',
+    maxSpeed: 120
+}
+console.log(motorbike);
+
+const bird = {
+    name: 'doyal',
+    color: 'white and black',
+    food: 'pokamakor'
+}
+console.log(bird);
+
+const laptop = {
+    brandName: 'lenovo',
+    processor: 'i3 12th gen',
+    ram: 16,
+    price: 55000,
+    displaySize: 15.6
+}
+console.log(laptop);
