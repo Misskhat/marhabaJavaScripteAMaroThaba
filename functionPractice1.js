44081de673d9828363625aabf8482227c146e0f0
unknown <misskhat.hossen@gmail.com>
2025-05-30 21:11:34 +0600

    first function practice updated

---
+++ functionPractice1.js
@@ -0,0 +1,53 @@
+function jusKhaw() {
+    console.log('ai naw labur jus');
+}
+jusKhaw()
+
+function myRoutine() {
+    console.log('gum thake utha, pani khai');
+    console.log('fresh hoi');
+    console.log('nasta kori');
+    console.log('office er jonno ready hoi');
+}
+myRoutine()
+
+function myFamily() {
+    console.log('my name: Ami');
+    console.log('my father name: Abbu');
+    console.log('my mother name: Ammu');
+    console.log('my brother name: Shoto Bhai');
+    console.log('my sister name: Shoto Bon');
+}
+myFamily()
+
+function taskDone() {
+    console.log('gum thake utha');
+    console.log('study kora');
+    console.log('office er kaj kora');
+    console.log('namaj pora');
+    console.log('family ke time daoya');
+}
+taskDone()
+
+function greetMe() {
+    console.log('Good Morning, Have a great day!');
+}
+greetMe()
+
+function getFavFriends() {
+    console.log('shipon');
+    console.log('humayon');
+    console.log('leon');
+}
+getFavFriends()
+
+function introduceMyself() {
+    console.log('my name: Md. Misskhat Hossain Milon');
+    console.log('my age: 30');
+    console.log('khilkhet, Dhaka, Bangladesh');
+    console.log('01754804496');
+    console.log('5.6F');
+    console.log('Alu vorta');
+}
+introduceMyself()
+
