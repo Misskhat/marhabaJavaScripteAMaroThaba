f3c427fe0352134964938b2a66befc9a300912ab
unknown <misskhat.hossen@gmail.com>
2025-06-02 20:31:34 +0600

    function pramether practice file create and two practice added

---
+++ functionPrameterPractice.js
@@ -0,0 +1,12 @@
+function averageCalculator(a, b) {
+    const sum = a + b;
+    const ave = sum / 2;
+    console.log(ave);
+}
+averageCalculator(20, 50);
+
+function multipli(a, b) {
+    const multi = a * b;
+    console.log(multi);
+}
+multipli(20, 15)
