97252310c6309ca3b73af1116944d6258e10357c
unknown <misskhat.hossen@gmail.com>
2025-06-03 14:51:29 +0600

    function parameter practice complete and update

--- functionPrameterPractice.js
+++ functionPrameterPractice.js
@@ -9,4 +9,48 @@ function multipli(a, b) {
     const multi = a * b;
     console.log(multi);
 }
-multipli(20, 15)
+multipli(20, 15);
+
+function threeSubjectAverage(a, b, c) {
+    const sum = a + b + c;
+    const subjectAverage = sum / 3;
+    console.log(subjectAverage);
+}
+threeSubjectAverage(12, 43, 70);
+
+
+function ageCalculator(birthYear) {
+    let currentYear = 2025;
+    const myAge = currentYear - birthYear;
+    console.log(myAge);
+}
+ageCalculator(1996);
+
+function lauCalculator(money) {
+    const lauPrice = 35;
+    const lauPabo = money / lauPrice;
+    console.log(lauPabo);
+}
+lauCalculator(500);
+
+function fourNumberAverate(num1, num2, num3, num4) {
+    const sum = num1 + num2 + num3 + num4;
+    const sumAverage = sum / 4;
+    console.log(sumAverage);
+}
+fourNumberAverate(2, 2, 2, 2);
+
+function sellingPrice(buyingPrice) {
+    console.log(buyingPrice + 250);
+}
+sellingPrice(300);
+
+function hundredYears(birthYear) {
+    console.log(birthYear + 100);
+}
+hundredYears(1996);
+
+function mobileUseTime(time) {
+    console.log(time * 30);
+}
+mobileUseTime(10);
