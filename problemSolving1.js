d4f2649ec56cc5f6fc974428953504bd437bb09d
Md. Misskhat Hossen Milon <155262372+Misskhat@users.noreply.github.com>
2025-06-22 20:57:51 +0600

    problem solving 1

---
+++ problemSolving1.js
@@ -0,0 +1,50 @@
+function gojToMail(goj) {
+    const gojCovert = goj * 1760;
+    return gojCovert;
+}
+// const number = gojToMail(13);
+// console.log(number);
+
+function kilowatGhontaToKilokalori(kg) {
+    const convertNumber = kg * 860;
+    return convertNumber;
+}
+// const number1 = kilowatGhontaToKilokalori(5);
+// console.log(number1);
+
+function ghontaToSecond(n) {
+    const ghontaToMinute = n * 60;
+    const minuteToSecond = ghontaToMinute * 60;
+    return minuteToSecond;
+}
+
+// const number2 = ghontaToSecond(6);
+// console.log(number2);
+
+function sentimeterToMeter(s) {
+    const convert = s * 100;
+    return convert;
+}
+// const number3 = sentimeterToMeter(40);
+// console.log(number3);
+
+function inchToSentiMeter(i) {
+    const convertNumber = i * 2.54;
+    return convertNumber;
+}
+// const number4 = inchToSentiMeter(5.5);
+// console.log(number4);
+
+function poundToKiloGram(p) {
+    const convertNumber = p * 0.453;
+    return convertNumber
+}
+// const number5 = poundToKiloGram(5)
+// console.log(number5);
+
+function gojToMeter(g) {
+    const convert = g * 0.91;
+    return convert;
+}
+// const number6 = gojToMeter(6);
+// console.log(number6);
