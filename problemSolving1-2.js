031bef3bbaa7e2f957c1189e0b83afbb77edb4a2
Md. Misskhat Hossen Milon <misskhat.hossen@gmail.com>
2025-06-25 21:57:35 +0600

    problem solving 1-2 start

---
+++ problemSolving1-2.js
@@ -0,0 +1,59 @@
+function tax(incomeAmount) {
+    if (incomeAmount <= 50000) {
+        const tax = incomeAmount * 0.10;
+        return incomeAmount - tax;
+    } else if (incomeAmount > 50000 && incomeAmount <= 100000) {
+        const tax = incomeAmount * 0.20;
+        return incomeAmount - tax;
+    } else if (incomeAmount > 100000 && incomeAmount <= 200000) {
+        const tax = incomeAmount * 0.30;
+        return incomeAmount - tax;
+    } else if (incomeAmount > 200000) {
+        const tax = incomeAmount * 0.40;
+        return incomeAmount - tax;
+    }
+}
+// const ic = tax(200001);
+// console.log(parseFloat(ic).toFixed(2));
+
+function packageDeliveryCost(wt) {
+    if (wt < 10) {
+        return 100;
+    } else if (wt > 10 && wt < 20) {
+        return 300;
+    } else if (wt > 20 && wt < 50) {
+        return 1000;
+    } else if (wt > 50) {
+        return (wt * 100)
+    }
+}
+// const shipmentWeight = packageDeliveryCost(21);
+// console.log(shipmentWeight);
+
+function gradeChecker(marks) {
+    if (marks >= 80) {
+        return 'A';
+    } else if (marks > 69) {
+        return 'B';
+    } else if (marks > 59) {
+        return 'C';
+    } else if (marks > 49) {
+        return 'D';
+    } else {
+        return 'F'
+    }
+}
+// const point = gradeChecker(80);
+// console.log(point);
+
+function arrayLeapYearCount(arr) {
+    const leapYear = [];
+    for (const ar of arr) {
+        if (ar % 400 === 0) {
+            leapYear.push(ar)
+        }
+    }
+    return leapYear;
+}
+const year = arrayLeapYearCount([2100, 2400, 1900, 2052]);
+console.log(year);
